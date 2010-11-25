dafa64539b9c5f1962b3764cb828bd8cd2756971
NiX0n <BiggoronSword+GitHub@gmail.com>
2010-11-25 02:41:59 +0000

    Copied: home module to photos

---
+++ application/modules/photos/controllers/Abstract.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This abstract controller class defines functionality to be shared
+ * across all module controllers.
+ */
+abstract class Home_Controller_Abstract extends Zend_Controller_Action
+{
+	public function init()
+	{
+		// Place init() code you want executed
+		// for every contoller in module here
+	}
+	
+	public function __call($method, $args)
+	{
+		if ('Action' == substr($method, -6)) {
+			// If the action method was not found, forward to the index action
+			return $this->_forward('index');
+		}
+
+		// all other methods throw an exception
+		throw new Exception('Invalid method "' . $method . '" called');
+	}
+}
+
+?>
