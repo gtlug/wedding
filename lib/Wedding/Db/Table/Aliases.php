31ce6e75a2f4f30602ea027544a537bf179c8809
NiX0n <BiggoronSword+GitHub@gmail.com>
2010-12-02 01:27:22 +0000

    Added: Primary select/fetch methods

--- lib/Wedding/Db/Table/Aliases.php
+++ lib/Wedding/Db/Table/Aliases.php
@@ -11,4 +11,41 @@ class Wedding_Db_Table_Aliases extends Zend_Db_Table_Abstract
 	
 	protected $_referenceMap    = array(
 	);
+	
+	/**
+	 * 
+	 * @param string $alias
+	 * @param Zend_Db_Table_Select $select
+	 * @return Zend_Db_Table_Select
+	 */
+	public function selectAliases($alias, $select = null)
+	{
+		if(null === $select)
+		{
+			$select = $this->select();
+		}
+		
+		$alias = trim($alias, " %");
+		$alias = "%$alias%";
+		$select = $select->where('aliasList LIKE ?', $alias);
+		
+		return $select;
+	}
+	
+	/**
+	 * 
+	 * @param string $alias
+	 * @param Zend_Db_Table_Select $select
+	 * @return array|null
+	 */
+	public function fetchAliases($alias, $select = null)
+	{
+		$select = $this->selectAliases($alias, $select);
+		$aliasesRow = $this->fetchRow($select);
+		$aliases = preg_split("/\s*,\s*/");
+		
+		return $aliases;
+	}
+	
+	
 }
