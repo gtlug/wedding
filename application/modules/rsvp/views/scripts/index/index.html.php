9037fc27e45589cc156c3297c2e21fe8aded2898
NiX0n <BiggoronSword+GitHub@gmail.com>
2010-12-04 21:41:17 +0000

    Added: Thank you message including invite mailingName Added: Checked for food (in case of updating)

--- application/modules/rsvp/views/scripts/index/index.html.php
+++ application/modules/rsvp/views/scripts/index/index.html.php
@@ -1,9 +1,27 @@
+<h1>Thank You For RSVP-ing <?= $this->invite->mailingName ?>!</h1>
+
 <form method="post" action="/rsvp/index/do">
 <input type="hidden" class="hidden" name="inviteId" value="<?= $this->invite->inviteId ?>" />
 <table border="0" cellpadding="0" cellspacing="0">
+<thead>
+	<tr>
+		<th class="attending">Attending?</th>
+		<th class="guestName">Guest Name</th>
+		<th class="foodId">Food Choice</th>
+	</tr>
+</thead>
+<tfoot>
+	<tr>
+		<td colspan="3">
+			<a href="javascript:void()">Add Guest</a><br /><br />
+			<button type="submit" class="submit">Save Changes</button>
+		</td>
+	</tr>
+</tfoot>
+<tbody>
 <?php foreach($this->guests as $guest) { ?>
 	<tr class="guest">
-		<td class="attending">
+		<td class="attending" style="text-align: right;">
 			<input 
 				type="hidden" 
 				class="hidden"
@@ -33,24 +51,28 @@
 				/>
 		</td>
 		
-		<td class="foodId">
+		<td class="foodId" style="font-size: smaller;">
 		<?php foreach($this->foods as $food) { ?>
+
 			<input
 				id="foodId<?= $this->id()->increment() ?>"
 				type="radio"
 				class="radio"
 				name="foodId[]"
 				value="<?= $food->foodId ?>"
+				<?= $guest->foodId == $food->foodId ? 'checked="checked"' : '' ?>
 				/>
-			<label for="foodId<?= $this->id() ?>"><?= $food->foodName ?></label>
+			<label for="foodId<?= $this->id() ?>"><?= $food->foodName ?></label><br />
 		<?php } /*foreach(foods)*/ ?>
 		</td>
 		
 	</tr>
 <?php } ?>
+</tbody>
 </table>
 </form>
 
+<h3>Food choices</h3>
 <dl>
 <?php foreach($this->foods as $food) { ?>
 	<dt><?= $food->foodName ?></dt>
