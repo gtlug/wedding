e6f4ff1be00c0274c2aa722f5c47d95e109ef890
NiX0n <BiggoronSword+GitHub@gmail.com>
2011-02-02 22:33:20 +0000

    Added: Newline

--- application/layouts/default.html.php
+++ application/layouts/default.html.php
@@ -2,6 +2,7 @@
 <?php if(false) {?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php } /*Trick IDE*/ ?>
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
