08f09bff75211c8af951a7b28313f357fabbdab4
NiX0n <BiggoronSword+GitHub@gmail.com>
2011-02-02 22:16:23 +0000

    Added: Use of headLink() and requisite doctype() helpers

--- application/layouts/default.html.php
+++ application/layouts/default.html.php
@@ -1,9 +1,14 @@
+<?=$this->doctype(Zend_View_Helper_Doctype::XHTML1_TRANSITIONAL)?>
+<?php if(false) {?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php } /*Trick IDE*/ ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?= $this->layout()->site ?></title>
-<?= $this->headStyle()->prependStyle('@import url(/css/main.css);') ?>
+<?= $this->headLink()->prependStylesheet('/css/main.css') ?>
+
+<?= $this->headStyle() ?>
 <script src="/js/prototype.js" type="text/javascript" language="javascript"></script>
 <script src="/js/common.js" type="text/javascript" language="javascript"></script>
 <?= $this->headScript() ?>
