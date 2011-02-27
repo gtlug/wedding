f1d04a9eb50c45bbd9dd63768f1e88d87f179b8b
NiX0n <BiggoronSword+GitHub@gmail.com>
2011-02-27 21:12:50 +0000

    Copied: Home module (it's small) to info module to save on structure generation.

---
+++ application/modules/info/views/scripts/index/index.html.php
@@ -0,0 +1,35 @@
+<?php 
+$this->headTitle('Welcome');
+$this->headLink()->appendStylesheet('/css/home.css');
+?>
+<div id="outer">
+
+	<div id="left">
+		<h1>Lindsey White &amp;<br/>Adam VanBerlo<br />Wedding</h1>
+		<p>Picture</p>
+	</div>
+	<div id="right">
+		<div id="top">
+			<div id="rsvp">
+<?= $this->action('widget', 'index', 'rsvp') ?>
+			</div>
+		</div>
+		<div id="bottom">
+			<p style="float: left;" id="registryLink">
+				<a 
+					href="http://www.bedbathandbeyond.com/regGiftRegistry.asp?wrn=-1622868992&"
+					title="Visit Registry"
+					target="_blank">
+					<img 
+						src="/images/home/bedBathBeyondLogo.jpg" 
+						alt="Visit Registry" 
+						width="33%" />
+				</a>
+			</p>
+			<p><a href="/photos">View Photos</a></p>
+			<div id="photos" style="padding-left: 20px; clear: left;">
+<?= $this->action('widget', 'index', 'photos') ?>
+			</div>		
+		</div>
+	</div>
+</div>
