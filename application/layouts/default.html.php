c0b963c900497b479bcc3edf7fd8859b0204231a
NiX0n <BiggoronSword+GitHub@gmail.com>
2011-02-28 14:36:58 +0000

    Changed: Moved headTitle() specifics to where the helper is actually called in layout

--- application/layouts/default.html.php
+++ application/layouts/default.html.php
@@ -1,9 +1,3 @@
-<?php 
-$this
-	->headTitle($this->layout()->site)
-	->setSeparator(' | ')
-;
-?>
 <?=$this->doctype(Zend_View_Helper_Doctype::XHTML1_TRANSITIONAL)?>
 <?php if(false) {?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -12,7 +6,7 @@ $this
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<?= $this->headTitle() ?>
+<?= $this->headTitle($this->layout()->site)->setSeparator(' | ') ?>
 
 <?= $this->headLink()->prependStylesheet('/css/main.css') ?>
 
