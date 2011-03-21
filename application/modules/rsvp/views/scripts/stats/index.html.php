d9487228655d4f66245157f14c4b27340be4e6c2
NiX0n <BiggoronSword+GitHub@gmail.com>
2011-03-21 21:34:30 +0000

    Added: Invite count Changed: Attending to friendly yes/no

--- application/modules/rsvp/views/scripts/stats/index.html.php
+++ application/modules/rsvp/views/scripts/stats/index.html.php
@@ -7,6 +7,7 @@ $adhocCount = 0;
 $foodSelectionCounts = array();
 $remainders = array();
 $attending = 0;
+$rowspans = array();
 
 foreach($this->guests as $guest)
 {
@@ -17,6 +18,12 @@ foreach($this->guests as $guest)
 	
 	if($guest->inviteId)
 	{
+		if(!isset($rowspans[$guest->inviteId]))
+		{
+			$rowspans[$guest->inviteId] = 0;
+		}
+		$rowspans[$guest->inviteId]++;
+		
 		if(!isset($remainders[$guest->inviteId]))
 		{
 			$remainders[$guest->inviteId] = $guest->guests;
@@ -40,9 +47,13 @@ foreach($this->guests as $guest)
 	}
 
 }
+$inviteCount = count($remainders);
 $declines = array_sum($remainders);
 ?>
 <dl>
+	<dt>Invite Response Count</dt>
+	<dd><?= $inviteCount ?></dd>
+
 	<dt>Total Named Guest Response Count</dt>
 	<dd><?= $guestsCount ?></dd>
 	
@@ -62,7 +73,7 @@ $declines = array_sum($remainders);
 	<dd>
 		<dl>
 <?php foreach($this->foods as $food) { $foodId = $food->foodId; ?>
-			<dt><?= $food->foodName ?></dt>
+			<dt>(<?= $foodId ?>) <?= $food->foodName ?></dt>
 			<dd><?= isset($foodSelectionCounts[$foodId]) ? $foodSelectionCounts[$foodId] : 0 ?></dd>
 			
 <?php } /*foreach(foods)*/?>
@@ -74,23 +85,34 @@ $declines = array_sum($remainders);
 <table border="1" cellpadding="5" cellspacing="0">
 <thead>
 	<tr>
-		<th>guestId</th>
-		<th>inviteId</th>
+		<th>dateUpdated</th>
 		<th>guestName</th>
 		<th>attending</th>
 		<th>foodId</th>
-		<th>dateUpdated</th>
+		<th>guestId</th>
+		<th>inviteId</th>
+		<th>expected</th>
 	</tr>
 </thead>
 <tbody>
 <?php foreach($this->guests as $guest) { ?>
 	<tr>
-		<td><?= $guest->guestId ?></td>
-		<td><?= $guest->inviteId ?></td>
-		<td><?= $guest->guestName ?></td>
-		<td><?= $guest->attending ?></td>
-		<td><?= $guest->foodId ?></td>
 		<td><?= $guest->dateUpdated ?></td>
+		<td><?= $guest->guestName ?></td>
+		<td><?= $guest->attending ? 'Yes' : 'No' ?></td>
+		<td><?= $guest->foodId ?></td>
+		<td><?= $guest->guestId ?></td>
+<?php if($guest->inviteId && isset($rowspans[$guest->inviteId])) { 
+	if(false !== $rowspans[$guest->inviteId]) { ?>
+		<td rowspan="<?= $rowspans[$guest->inviteId] ?>"><?= $guest->inviteId ?></td>
+		<td rowspan="<?= $rowspans[$guest->inviteId] ?>"><?= $guest->guests ?></td>
+<?php
+ 		$rowspans[$guest->inviteId] = false;
+	} /*if(null !== $rowspans[$guest->inviteId]))*/		
+} else { /*if(isset($rowspans[$guest->inviteId])) {*/ ?>		
+		<td><?= $guest->inviteId ?></td>
+		<td><?= $guest->guests ?></td>
+<?php } /*if(isset($rowspans[$guest->inviteId]))...else {*/ ?>
 	</tr>
 <?php } /*foreach(guests) {*/ ?>
 </tbody>
